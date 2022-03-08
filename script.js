5ce82552afa3e421986d3d3d8970d8086a04f1ca
Anshul Singh <singhanshul2807@gmail.com>
2022-03-09 01:40:10 +0530

    validator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let msg=document.getElementById('user-input');
 
 function sendMessage(){
+    event.preventDefault()
     let curr_msg=msg.value;
     let chatBody=document.getElementById('chat-body');
     reset(msg);
@@ -8,8 +9,20 @@ function sendMessage(){
     let text = document.createTextNode(curr_msg);
     tagP.appendChild(text);
     tagP.classList.add("user-chat");
+    if(isValidHttpUrl(curr_msg))    tagP.classList.add("link-contains")
     chatBody.appendChild(tagP);
 }
 function reset(param){
     param.value="";
 }
+function isValidHttpUrl(string) {
+    let url;
+    
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+  
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
